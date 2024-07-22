b1817bf0b4ac79da63f7c677b5b77fff60b65dad
Pransu-code <pransu54321@gmail.com>
2024-07-22 18:29:47 +0530

    Addded income IN OUT and interests

--- script.js
+++ script.js
@@ -76,3 +76,21 @@ const account1 = {
   };
 
   displayMovements(account1.movements);
+
+  const calcDisplaySummary = function(movements) {
+    
+    const incomes = movements.filter(mov => mov > 0).reduce((acc,mov) => acc+mov,0);
+    console.log(incomes);
+    labelSumIn.textContent = `${incomes}`;
+    
+  
+  
+    const out = movements.filter(mov => mov<0).reduce((acc,mov)=> acc+mov, 0);
+
+    labelSumOut.textContent = `${Math.abs(out)}`;
+
+    const interest = movements.filter(mov => mov > 0).map(deposit => (deposit*1.2)/100).reduce((acc,mov) => acc+mov,0);
+
+    labelSumInterest.textContent = `${interest}`;
+  }
+calcDisplaySummary(account1.movements);
